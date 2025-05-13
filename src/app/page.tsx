9cf1fa15c4d4ef4e103954a7e2e386799b7c5e8f
Emmanuel Uwem <emmanueluwem560@gmail.com>
2025-05-13 22:16:55 +0100

    Add site manifest and logo image for Sonic Wall application
    - Created a new web manifest file for the Sonic Wall application, defining its name, icons, theme color, and display settings.
    - Added a logo image for the Sonic Wall application, created using Adobe Photoshop Express.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,103 +1,56 @@
+"use client"
 import Image from "next/image";
+import Link from "next/link";
+import { usePathname } from "next/navigation";
+
 
 export default function Home() {
-  return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm/6 text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2 tracking-[-.01em]">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-[family-name:var(--font-geist-mono)] font-semibold">
-              src/app/page.tsx
-            </code>
-            .
-          </li>
-          <li className="tracking-[-.01em]">
-            Save and see your changes instantly.
-          </li>
-        </ol>
+  const pathName = usePathname();
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
+  const navLinks = [
+    { title: "Dashboard", href: "/", icon: "" },
+    { title: "Rules", href: "/dashboard/rules", icon: "" },
+    { title: "Analytics", href: "/dashboard/analytics", icon: "" },
+    { title: "Integration", href: "/dashboard/integration", icon: "" },
+    { title: "Settings", href: "/dashboard/settings", icon: "" },
+  ];
+  return (
+    <main className="flex w-full h-full bg-[#1A1F29] relative">
+      <div className="flex flex-col justify-start items-start p-4 bg-[#061224] w-[20vw]">
+        <h1 className="flex justify-start items-center gap-3 font-bold text-2xl">
+          <span className="relative h-14 w-14 rounded-2xl overflow-hidden">
             <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 w-full sm:w-auto md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+              alt=" logo"
+              src={"/sonic wal logo.jpg"}
+              layout="fill"
+              objectFit="contain"
+              objectPosition="center"
+            ></Image>
+          </span>
+          Sonic Wall
+        </h1>
+        <nav
+          className={`flex flex-col w-full justify-start items-start gap-2 rounded-2xl py-6 px-3`}
+        >
+          {navLinks.map((link) => (
+            <Link
+              href={link.href}
+              key={link.title}
+              className={`text-base opacity-90 py-2 px-4 font-medium w-full rounded-lg ${
+                link.href === pathName ? "bg-[#1E2A38] text-[#4DA8DA]" : ""
+              }`}
+            >
+              {link.title}
+            </Link>
+          ))}
+        </nav>
+      </div>
+      <div className="flex flex-col w-full justify-start items-start p-4 sm:p-6">
+        <div className="flex justify-between items-center w-full">
+          <h2 className="text-lg font-semibold">Dashboard</h2>
+          <button className="p-3 rounded-lg border border-gray-600 hover:opacity-80 cursor-pointer"> Connect Wallet</button>
         </div>
-      </main>
-      <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
+      </div>
+    </main>
   );
 }
